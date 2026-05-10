1515561f867044309e17b76603bc10b3dfd23de9
ali samet <samet.fb74@gmail.com>
2026-05-10 15:24:34 +0300

    Şıklı modlar eklenmeden önce

--- Project_HafizA.Web/obj/Debug/net8.0/Project_HafizA.Web.AssemblyInfo.cs
+++ Project_HafizA.Web/obj/Debug/net8.0/Project_HafizA.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_HafizA.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101fa56a024e67cc0476c6582d3b29f6d7b5045b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_HafizA.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_HafizA.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
